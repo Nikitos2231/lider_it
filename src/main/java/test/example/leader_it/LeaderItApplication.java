67cac097af7725681eaa3291ddd30d8ffc011ad5
Nikita <nik.porshennikov@mail.ru>
2023-03-17 20:10:49 +0700

    init start state for project

---
+++ src/main/java/test/example/leader_it/LeaderItApplication.java
@@ -0,0 +1,14 @@
+package test.example.leader_it;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderItApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/test/example/leader_it/LeaderItApplicationTests.java
@@ -0,0 +1,13 @@
+package test.example.leader_it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
